fa2afd962bdb435988b3b6a1722bbc024275086d
jotage <joaoguimatz@gmail.com>
2024-11-21 19:57:47 -0300

    melhoras

--- js/contraste.js
+++ js/contraste.js
@@ -2,3 +2,17 @@ const botaoContraste = document.getElementById('modo-contraste');
 botaoContraste.addEventListener('click', () => {
     document.body.classList.toggle('alto-contraste');
 });
+
+const modoLeituraButton = document.getElementById('modo-leitura');
+
+function lerTexto(texto) {
+    const synth = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(texto);
+    utterance.lang = 'pt-BR';
+    synth.speak(utterance);
+}
+
+modoLeituraButton.addEventListener('click', () => {
+    const textoParaLer = document.body.innerText;
+    lerTexto(textoParaLer);
+});
--- js/leitura.js
+++ js/leitura.js
@@ -2,20 +2,17 @@ const botaoContraste = document.getElementById('modo-contraste');
 botaoContraste.addEventListener('click', () => {
     document.body.classList.toggle('alto-contraste');
 });
-// Seleciona o botão de ativar modo leitura
+
 const modoLeituraButton = document.getElementById('modo-leitura');
 
-// Função para ler o texto da página
 function lerTexto(texto) {
     const synth = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(texto);
-    utterance.lang = 'pt-BR'; // Define o idioma como português do Brasil
+    utterance.lang = 'pt-BR';
     synth.speak(utterance);
 }
 
-// Evento de clique para o botão "Ouvir Página"
 modoLeituraButton.addEventListener('click', () => {
-    // Seleciona todo o texto da página que deve ser lido
     const textoParaLer = document.body.innerText;
     lerTexto(textoParaLer);
 });
